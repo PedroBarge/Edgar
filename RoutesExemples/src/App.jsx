47b6c91e82918d0eefcefd0103a83adbbe11ffdf
PedroBarge <pedro525tavares@gmail.com>
2024-03-25 19:29:40 +0000

    route to home is working. now has a logo too

--- RoutesExemples/src/App.jsx
+++ RoutesExemples/src/App.jsx
@@ -4,26 +4,28 @@
 import Navigation from "/src/Componets/Navigation/Navigation.jsx";
 
 import "./App.css";
+import Icon from "/src/assets/img/Designer.png";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 import Shop from "./Routes/jsx/Shop";
+import Home from "./Routes/jsx/Home";
 //----------------------------------------------------------------
 
 function App() {
-  
   return (
     <>
+      <img style={{ width: "25%", height: "100%" }} src={Icon} alt="IconHome" />
+      <h1>Unique Cats Store</h1>
+      <h3>MIAUU</h3>
       <Router>
         <Navigation />
         <Routes>
           <Route path="/" exact element={""} />
+          <Route path="/home" element={<Home />} />
           <Route path="/shop" element={<Shop />} />
         </Routes>
       </Router>
-      <h1>Loja de Gatos</h1>
-      <h3>MIAUU</h3>
-      <p>+---------------------------------------------+</p>
     </>
   );
 }
--- RoutesExemples/src/Componets/Navigation/Navigation.jsx
+++ RoutesExemples/src/Componets/Navigation/Navigation.jsx
@@ -1,5 +1,9 @@
-import "../../assets/css/nav.css";
+import "./nav.css";
+
 import { Link } from "react-router-dom";
+
+import HomeImg from "/src/assets/img/botao-de-inicio.png"
+import ShopImg from "/src/assets/img/pet-shop.png"
 
 
 const Navigation = () => {
@@ -8,13 +12,13 @@ const Navigation = () => {
       <nav className="navbar">
         <ul className="nav-list">
           <li>
-            <Link to="/">
-              <img src="" alt="Home Page" className="my-img" />
+            <Link to="/home">
+              <img src={HomeImg} alt="Home Page" className="my-img" />
             </Link>
           </li>
           <li>
             <Link to="/shop">
-              <img src="" alt="Shop" className="my-img" />
+              <img src={ShopImg} alt="Shop" className="my-img" />
             </Link>
           </li>
         </ul>
