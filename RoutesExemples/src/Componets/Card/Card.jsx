b711bd9b8018eb8496e90ac185e790bc04ddbabb
PedroBarge <pedro525tavares@gmail.com>
2024-03-26 12:20:01 +0000

    re organize how the cats appear

--- RoutesExemples/src/Componets/Card/Card.jsx
+++ RoutesExemples/src/Componets/Card/Card.jsx
@@ -1,17 +1,39 @@
 /* eslint-disable react/prop-types */
 const Card = ({ race, name, color, weight, age, location, image, price }) => {
-    return (
-      <div>
-        <img src={image} alt={name} />
-        <h2>Name: {name}</h2>
-        <p>Race: {race}</p>
-        <p>Color: {color}</p>
-        <p>Weight: {weight}</p>
-        <p>Age: {age}</p>
-        <p>Location: {location}</p>
-        <p>Price: {price}</p>
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          columnGap: "10px",
+          rowGap: "10px",
+          padding: "2em",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <div
+          style={{
+            height: "100%",
+            width: "65%",
+          }}
+        >
+          <img src={image} alt={name} />
+        </div>
+        
+        <div>
+          <h2>{name}</h2>
+          <p>Race: {race}</p>
+          <p>Color: {color}</p>
+          <p>Weight: {weight}</p>
+          <p>Age: {age}</p>
+          <p>Location: {location}</p>
+          <p>Price: {price}</p>
+        </div>
       </div>
-    );
-  };
+    </>
+  );
+};
 
-  export default Card;
+export default Card;
